97ab4873a9cfc1781cb0761c15b8caffbd8e931e
Pranay Raj <58875281+masterpranay1@users.noreply.github.com>
2023-10-23 04:56:43 +0000

    Refactor : app.js ( cors, lint )

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require("express");
-const app = express();
-const cookieParser=require('cookie-parser')
-app.use(cookieParser());
-const dotenv = require('dotenv').config();
+const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8000;
-const connectDB = require("./config/db");
-const bcrypt =require('bcrypt');
-connectDB();
-const authRoute = require("./routes/authRoute");
-const postRoute = require("./routes/postRoute");
+const cors = require('cors')
 
+require('dotenv').config();
+
+const postRoute = require("./routes/postRoute");
+const authRoute = require("./routes/authRoute");
+
+const connectDB = require("./config/db");
+connectDB();
+
+const app = express();
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const PORT = process.env.PORT || 8000;
+const corsOption = {
+    credentials: true,
+    origin: [
+        "*"
+    ],
+};
 
 // routes
+app.use(cors(corsOption))
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/post', postRoute);
 
-
-
-
-
-app.listen(port, () => {
-    console.log(`app is listening at port ${port}`);
+app.listen(PORT, () => {
+    console.log(`app is listening at port ${PORT}`);
 })
 
+// base
+app.get("/", (req, res) => {
+    res.status(200).json({ msg: "Wiz Out" });
+});
