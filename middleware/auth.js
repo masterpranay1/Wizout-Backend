7e41248d3da27d1c22325745cc1eef3bbe4af2cb
Pranay Raj <58875281+masterpranay1@users.noreply.github.com>
2023-10-23 06:16:37 +0000

    Refactor ( post ) : cjs to module

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,19 @@
-const express=require('express');
-const app=express();
-const jwt=require('jsonwebtoken');
-const user=require('../models/authModel');
-const cookieParser=require('cookie-parser')
-app.use(cookieParser())
-exports.auth=async(req,res,next)=>{
-try{
-    const tokens=req.cookies.token;
-    if(!tokens){
-        res.status(404).json({
-            message:"Please Login to access the resource"
-        })
-    }else{
+import jwt from "jsonwebtoken";
+import user from '../models/authModel'
 
-        const decode=await jwt.verify(tokens,process.env.SECRET_KEY)
-        req.user=await user.find({primary_email:decode.primary_email});
-        next();
+export const auth = async (req, res, next) => {
+  try {
+    const tokens = req.cookies.token;
+    if (!tokens) {
+      res.status(404).json({
+        message: "Please Login to access the resource",
+      });
+    } else {
+      const decode = jwt.verify(tokens, process.env.SECRET_KEY);
+      req.user = await user.find({ primary_email: decode.primary_email });
+      next();
     }
-}catch(err){
+  } catch (err) {
     return next(new Error("please Login First"));
-}
-}
+  }
+};
