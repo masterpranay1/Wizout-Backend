c0cc6dc2bbdefda7924ba97c368efdb263b7e74a
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-03 09:54:29 +0530

    Feat : Auth Controller

---
+++ controller/authController.js
@@ -0,0 +1,30 @@
+import User from '../models/user.js';
+
+class AuthController {
+  // Method for handling user registration
+  async register(req, res) {
+    
+  }
+
+  // Method for handling user login
+  async login(req, res) {
+    
+  }
+
+  // Method for handling user logout
+  async logout(req, res) {
+    // Implementation goes here
+  }
+
+  // Method for handling password reset request
+  async forgotPassword(req, res) {
+    // Implementation goes here
+  }
+
+  // Method for handling password reset confirmation
+  async resetPassword(req, res) {
+    // Implementation goes here
+  }
+}
+
+export default AuthController;
