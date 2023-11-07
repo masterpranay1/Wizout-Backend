71606ec68d1817fdd6d7fa66cb2e0cd428a71f94
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-07 10:12:54 +0530

    Refactor : Auth Route and Controller

--- controller/authController.js
+++ controller/authController.js
@@ -1,14 +1,30 @@
-import User from '../models/user.js';
+import UserService from '../services/userService.js';
 
 class AuthController {
   // Method for handling user registration
   async register(req, res) {
-    
+    try {
+      const { uid, name, email, password } = req.body;
+      const existingUser = await UserService.checkUser(email);
+      if (existingUser) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+      const user = await UserService.createUser(uid, name, email, password);
+      res.status(201).json({ user });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 
   // Method for handling user login
   async login(req, res) {
-    
+    try {
+      const { email, password } = req.body;
+      const user = await UserService.loginUser(email, password);
+      res.status(200).json({ user });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
 
   // Method for handling user logout
@@ -27,4 +43,4 @@ class AuthController {
   }
 }
 
-export default AuthController;
+export default new AuthController();
