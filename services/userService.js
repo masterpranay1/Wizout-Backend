e743ad6f81014f728bbad66289375dce95dc8768
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-12 14:28:36 +0530

    Update (user service) - check unverified user - save otp - update user methods added

--- services/userService.js
+++ services/userService.js
@@ -31,6 +31,56 @@ class UserService {
       return error;
     }
   }
+
+  // check if a user exists in the database but not verified
+  async checkUnverifiedUser(email) {
+    try {
+      const user = await User.findOne({ email, verified: false });
+      if (user) {
+        console.log('Unverified user exists in the database');
+        return user;
+      } else {
+        console.log('Unverified user does not exist in the database');
+        return null;
+      }
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
+
+  // save otp to the database
+  async saveOtp(email, otp) {
+    try {
+      const user = await User.findOneAndUpdate({ email }, { otp });
+      if (user) {
+        console.log('OTP saved successfully');
+        return user;
+      } else {
+        console.log('Failed to save OTP');
+        return null;
+      }
+    } catch(error) {
+      console.error(error);
+      return error;
+    }
+  }
+
+  async updateUser(email, data) {
+    try {
+      const user = await User.findOneAndUpdate({ email }, data, { new: true });
+      if (user) {
+        console.log('User updated successfully');
+        return user;
+      } else {
+        console.log('User does not exist in the database');
+        return null;
+      }
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
+  }
 }
 
 export default new UserService();
