f095e7d2a0c17e53f980ac5631426c55e0d20554
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-12 14:30:33 +0530

    Refactor ( auth route ) : Added 2 routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,6 +3,8 @@ import AuthController from "../controller/authController.js";
 const router = express.Router();
 
 router.post("/register", AuthController.register);
+router.post("/send-verification-email", AuthController.sendVerificationEmail);
+router.post("/verify-otp", AuthController.verifyOTP);
 router.post("/login", AuthController.login);
 
 export default router;
