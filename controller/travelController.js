613f9b96f5b20389f210d3efcbfd2a0c24e47969
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-23 19:29:32 +0530

    Update ( Travel Controller ) : New Methods

--- controller/travelController.js
+++ controller/travelController.js
@@ -1,4 +1,5 @@
 import TripService from "../services/tripService.js";
+import UserService from "../services/userService.js";
 
 class TravelController {
   async createTrip(req, res) {
@@ -13,7 +14,9 @@ class TravelController {
       budget,
     };
     try {
-      const newTrip = await TripService.addTrip(trip);
+      const newTrip = await TripService.addTrip(trip, userId);
+      const tripId = newTrip._id;
+      await UserService.addTravelId(userId, tripId);
       res.status(201).json(newTrip);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -57,12 +60,16 @@ class TravelController {
         throw new Error("You are the owner of this trip");
       }
 
-      const isInTravelers = await TripService.checkIdInTravelers(userId, tripId);
+      const isInTravelers = await TripService.checkIdInTravelers(
+        userId,
+        tripId
+      );
       if (isInTravelers) {
         throw new Error("You are already in the travelers");
       }
 
       const trip = await TripService.sendRequest(userId, tripId);
+      await UserService.addRequestId(userId, tripId);
       res.status(200).json(trip);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -89,17 +96,25 @@ class TravelController {
         throw new Error("You are not the owner of this trip");
       }
 
-      const isInTravelers = await TripService.checkIdInTravelers(acceptId, tripId);
+      const isInTravelers = await TripService.checkIdInTravelers(
+        acceptId,
+        tripId
+      );
       if (isInTravelers) {
         throw new Error("This user is already in the travelers");
       }
 
-      const isInRequests = await TripService.checkIdInRequests(acceptId, tripId);
+      const isInRequests = await TripService.checkIdInRequests(
+        acceptId,
+        tripId
+      );
       if (!isInRequests) {
         throw new Error("This user is not in the requests");
       }
 
       const trip = await TripService.acceptRequest(acceptId, tripId);
+      await UserService.addTravelId(acceptId, tripId);
+      await UserService.removeRequestId(acceptId, tripId);
       res.status(200).json(trip);
     } catch (error) {
       res.status(500).json({ error: error.message });
@@ -113,13 +128,47 @@ class TravelController {
         throw new Error("You are not the owner of this trip");
       }
 
-      const isInRequests = await TripService.checkIdInRequests(acceptId, tripId);
+      const isInRequests = await TripService.checkIdInRequests(
+        acceptId,
+        tripId
+      );
       if (!isInRequests) {
         throw new Error("This user is not in the requests");
       }
 
       const trip = await TripService.rejectRequest(acceptId, tripId);
+      await UserService.removeRequestId(acceptId, tripId);
       res.status(200).json(trip);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async getAllTravelIdOfUser(req, res) {
+    const { userId } = req.body;
+    try {
+      const trips = await TripService.getAllTravelIdOfUser(userId);
+      res.status(200).json(trips);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async getAllRequestIdOfUser(req, res) {
+    const { userId } = req.body;
+    try {
+      const trips = await TripService.getAllRequestIdOfUser(userId);
+      res.status(200).json(trips);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async getAllTravellers(req, res) {
+    const { tripId } = req.body;
+    try {
+      const travellers = await TripService.getAllTravellers(tripId);
+      res.status(200).json(travellers);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
