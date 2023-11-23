5f19695a11f830a47dfd9f9c5f4a3b0022e3b588
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-23 11:04:08 +0530

    Feat : Travel ( Trip ) Route

--- routes/travelRoute.js
+++ routes/travelRoute.js
@@ -1,9 +1,38 @@
 import express from "express";
+import TravelController from "../controller/travelController.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Travel Route!!");
 });
+
+// @trips api
+
+// TODO: Verify is user is logged in
+
+// Create a trip
+router.post("/create-trip", TravelController.createTrip);
+
+// Get all trips
+router.get("/get-all-trips-id", TravelController.getAllTripsId);
+
+// get all trips id of a user
+router.post("/get-all-trips-id-of-user", TravelController.getAllTripsIdOfUser);
+
+// Get a trip by id
+router.post("/get-trip-by-id", TravelController.getTripById);
+
+// send a request to join a trip
+router.post("/send-request", TravelController.sendRequest);
+
+// get all requests of a certain trip
+router.post("/get-all-requests", TravelController.getAllRequests);
+
+// accept a request
+router.post("/accept-request", TravelController.acceptRequest);
+
+// reject a request
+router.post("/reject-request", TravelController.rejectRequest);
 
 export default router;
