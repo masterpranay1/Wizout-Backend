a44200ce9bc108a5cb4c9dcc8744ff03b382dd92
masterpranay1 <rajpranaychottu@gmail.com>
2023-11-23 11:05:05 +0530

    Feat : Trip Service

---
+++ services/tripService.js
@@ -0,0 +1,65 @@
+import TripModel from "../models/tripModel.js";
+
+class TripService {
+  async addTrip(trip) {
+    console.log(trip);
+    const newTrip = new TripModel({
+      ...trip,
+      requests: [],
+      travelers: [],
+    });
+    await newTrip.save();
+    return newTrip;
+  }
+
+  async getTripById(id) {
+    const trip = await TripModel.findById(id);
+    return trip;
+  }
+
+  async getAllTripsId(filter) {
+    const trips = await TripModel.find({
+      ...filter,
+    }).select("_id");
+    return trips;
+  }
+
+  async deleteTripById(id) {
+    const deletedTrip = await TripModel.findByIdAndDelete(id);
+    return deletedTrip;
+  }
+
+  async sendRequest(userId, tripId) {
+    const trip = await TripModel.findById(tripId);
+    trip.requests.push(userId);
+    await trip.save();
+    return trip;
+  }
+
+  async getAllRequests(tripId) {
+    const trip = await TripModel.findById(tripId);
+    return trip.requests;
+  }
+
+  async acceptRequest(userId, tripId) {
+    const trip = await TripModel.findById(tripId);
+    trip.requests = trip.requests.filter((request) => request.toString() !== userId);
+    trip.travelers.push(userId);
+    await trip.save();
+    return trip;
+  }
+
+  async rejectRequest(userId, tripId) {
+    const trip = await TripModel.findById(tripId);
+    trip.requests = trip.requests.filter((request) => request.toString() !== userId);
+    await trip.save();
+    return trip;
+  }
+
+  async isOwner(userId, tripId) {
+    const trip = await TripModel.findById(tripId);
+    return trip.userId.toString() === userId;
+  }
+}
+
+export default new TripService();
