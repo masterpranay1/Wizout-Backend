760ad63e34aef39891f28bc9e52af6df0514f255
Pranay Raj <masterpranayraj@gmail.com>
2024-01-25 11:36:21 +0530

    Refactor app.js and add SocketService

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-import express from 'express'
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
 
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
 /* ************* */
 /* Local Imports */
 /* ************* */
 
-import connectDB from './config/db.js'
+import SocketService from "./services/socketService.js";
+import connectDB from "./config/db.js";
 connectDB();
+const socketService = new SocketService();
 
 /* ************* */
 /* Routes Import */
 /* ************* */
 
-import authRoute from './routes/authRoute.js'
+import authRoute from "./routes/authRoute.js";
 // import postRoute from './routes/postRoute.js'
-import travelRoute from './routes/travelRoute.js'
+import travelRoute from "./routes/travelRoute.js";
 
 /* ***************** */
 /* Express APP Logic */
@@ -26,28 +29,31 @@ import travelRoute from './routes/travelRoute.js'
 
 const PORT = process.env.PORT || 8000;
 const corsOption = {
-    credentials: true,
-    origin: [
-        "*"
-    ],
+  credentials: true,
+  origin: ["*"],
 };
 
 const app = express();
 
-app.use(cors(corsOption))
+app.use(cors(corsOption));
 app.use(cookieParser());
 app.use(express.json());
 
 // routes
-app.use('/api/v1/auth', authRoute);
+app.use("/api/v1/auth", authRoute);
 // app.use('/api/v1/post', postRoute);
-app.use('/api/v1/travel', travelRoute);
+app.use("/api/v1/travel", travelRoute);
 
-app.listen(PORT, () => {
-    console.log(`app is listening at port ${PORT}`);
-})
+const server = http.createServer(app);
+socketService.io.attach(server);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+socketService.initListeners();
 
 // base
 app.get("/", (req, res) => {
-    res.status(200).json({ msg: "Wiz Out" });
+  res.status(200).json({ msg: "Wiz Out" });
 });
